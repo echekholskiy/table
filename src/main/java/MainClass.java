d5243189da72759b325bd0caa0e34d989b4075db
echekholskiy <62834709+echekholskiy@users.noreply.github.com>
2020-04-06 19:40:40 +0300

    Не работает клик через цикл for

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,30 +3,33 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import javax.swing.*;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainClass {
     static WebDriver driver;
+
     public static void main(String[] args) {
+        //MainClass m=new MainClass();
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\nazhivka.by\\IdeaProjects\\testselenium\\drivers\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("https://imarket.by/catalog/kholodilniki/");
-        clickCheckBox("Samsung");
-        clickCheckBox("LG");
+        driver.get("https://www.21vek.by/washing_machines/");
+        List<WebElement> lis= driver.findElements(By.xpath("//dt[text()='Производители']/parent::dl/parent::div//dd[@class='filter-attr__value']"));
+        //lis.get(3).click();
+        //List<WebElement> checkb=driver.findElements(By.xpath("//ul[@class='catalog-sort']/following-sibling::span"));
 
-
-
-
-
-    }
-    public static void clickCheckBox(String name){
-        String rbXpath="//span[text()='%s']";
-        if (!driver.findElement(By.xpath(String.format(rbXpath, name)+"/parent::label/preceding-sibling::input")).isSelected()){
-            driver.findElement(By.xpath(String.format(rbXpath, name))).click();
+        for (WebElement chechbox : lis){
+            chechbox.click();
         }
 
+
+
+
+
+
     }
+
 
 }
